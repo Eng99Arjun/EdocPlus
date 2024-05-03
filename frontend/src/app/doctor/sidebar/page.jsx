2e5beccabf77b23ed923e6e4b410273caad18e77
arush12345648 <arushkatiyar1234567899@gmail.com>
2024-05-03 10:56:30 +0530

    ...

--- frontend/src/app/doctor/sidebar/page.jsx
+++ frontend/src/app/doctor/sidebar/page.jsx
@@ -71,7 +71,7 @@ const Sidebar = () => {
           </li>
           <li>
             <a
-              href="/doctor/manageAppointment"
+              href="/doctor/doctor-appointments"
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <svg
