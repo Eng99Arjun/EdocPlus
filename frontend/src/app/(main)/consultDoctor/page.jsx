59c95db1cd248bcc768ba04dd767c77c3c6983fa
arush12345648 <arushkatiyar1234567899@gmail.com>
2024-05-06 11:08:29 +0530

    Refactor navbar and sidebar components to use relative links

--- frontend/src/app/(main)/consultDoctor/page.jsx
+++ frontend/src/app/(main)/consultDoctor/page.jsx
@@ -3,48 +3,10 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
 
 
-
-<>
-<div className="relative h-[400px] bg-gradient-to-tr from-indigo-600 via-indigo-700 to-violet-800">
-  <div className="flex flex-col gap-4 justify-center items-center w-full h-full px-3 md:px-0">
-    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white">
-      McqMate
-    </h1>
-    <p className="text-gray-300">For Students, From Students</p>
-    <div className="relative p-3 border border-gray-200 rounded-lg w-full max-w-lg">
-      <input
-        type="text"
-        className="rounded-md w-full p-3 "
-        placeholder="Search MCQ | Topic | Course"
-      />
-      <button type="submit" className="absolute right-6 top-6">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
-      </button>
-    </div>
-  </div>
-</div>
-
-
-
-
-</>
 const consultDoctor = () => {
 
     const [doctorList, setDoctorList] = useState([]);
-    const [filter, setfilter] = useState([])
+   const [filterList, setfilterList] = useState([])
 
     const fetchDoctors = () => {
         fetch('http://localhost:5000/doctor/getall')
@@ -54,7 +16,7 @@ const consultDoctor = () => {
             .then((data) => {
                 console.log(data);
                 setDoctorList(data);
-                setfilter(data);
+                setfilterList(data);
             })
             .catch((err) => {
                 console.log(err);
@@ -75,7 +37,7 @@ const consultDoctor = () => {
                         </div>
                         <h5 className='text-2xl md:text-3xl font-medium mt-3 text-center'>Dr. {doctor.name}</h5>
                         <p className="text-slate-500 text-center text:lg mt:3">{doctor.specialization}</p>
-                        <Link href={'/user/view-doctor/' + doctor._id} className='text-center  text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-700 hover:text-white focus:scale-95 transition-all bg-blue-500 text-white duration-200 ease-out'>View</Link>
+                        <Link href={'/user/view-doctor/' + doctor._id} className='text-center py-2 rounded-lg font-semibold mt-4 hover:bg-blue-700 hover:text-white focus:scale-95 transition-all bg-blue-500 text-white duration-200 ease-out'>View</Link>
                     </div>
                 </div>
             ))
@@ -84,15 +46,13 @@ const consultDoctor = () => {
         }
     }
 
-
     const filterDoctor = (e) => {
         const value = e.target.value;
-        setDoctorList(filter.filter = ((doctor) => {
-            return  (doctor.specialization.toLowerCase().includes(value.toLowerCase()));
+        setDoctorList(filterList.filter = ((doctor) => {
+            return (doctor.specialization.toLowerCase().includes(value.toLowerCase()));
         }))
     }
 
-  
     return (
         <div>
             <section className='pt-12'>
@@ -114,7 +74,7 @@ const consultDoctor = () => {
                             type="text"
                             placeholder="Search for Doctors, clinic or hospitals"
                             className="w-full md:w-80 px-3 h-10 rounded-l border-2 border-sky-500 focus:outline-none focus:border-sky-500"
-                        onChange={filterDoctor}
+                            onChange={filterDoctor}
                         />
                         <button
                             type="submit"
@@ -126,28 +86,7 @@ const consultDoctor = () => {
                 </form>
             </section>
 
-            {/* search bar ends */}
-
-            {/* card */}
-            {/* 
-        <div className='rounded-xl shadow-lg w-56 ml-3'>
-            <a href=""></a>
-                <div className='bg-blue-200 pl-3 pt-9 pr-9 pb-0 rounded-t-lg'>
-                <a href="">
-                    <span className=''>
-                        <img 
-                        src="video_consulation.webp"
-                        className='h-48 w-52' 
-                        alt="" />
-                    </span>
-                 </a>
-            </div>
-                <div className='p-3 row-span-1 text-2xl font-medium text-gray-700 font-serif'>
-                Instant Video 
-                <br />Solution
-            </div>
-        </div>
-          */}
+          
             <div className='flex items-center justify-center pt-12 pb-12 container mx-auto'>
 
                 <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
